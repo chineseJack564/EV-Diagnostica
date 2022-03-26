3642a8311ba617f790feb3aac075082198a8dac6
chineseJack564 <zyan@uc.cl>
2022-03-26 11:39:50 -0300

    top days and user

--- TopRetweet.py
+++
@@ -1,6 +0,0 @@
-def TopRetweet(tweetList):
-    """
-    Returns the most retweeted tweet in a list of tweets
-    """
-    order_by_retweet_count = tweetList.sort_values(by=['retweetCount'], ascending=False)
-    return order_by_retweet_count
