d8fb27f0120654294a5394fd7e25559700ff0dfb
chineseJack564 <zyan@uc.cl>
2022-03-26 10:46:24 -0300

    update

--- TopRetweet.py
+++ TopRetweet.py
@@ -3,5 +3,4 @@ def TopRetweet(tweetList):
     Returns the most retweeted tweet in a list of tweets
     """
     order_by_retweet_count = tweetList.sort_values(by=['retweetCount'], ascending=False)
-    print(order_by_retweet_count.head(10))
     return order_by_retweet_count
